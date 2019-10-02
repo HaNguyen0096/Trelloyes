36544b83d2eb87331790f824f94d17739b6dbe3c
HaNguyen0096 <37911930+HaNguyen0096@users.noreply.github.com>
2019-10-01 22:04:37 -0400

    Add files via upload

---
+++ src/List.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import List from './List';
+
+describe('Messages component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List name="List" unread={4}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+
+  it('renders the UI as expected with no unreads', () => {
+    const tree = renderer
+      .create(<List name="List" unread={0}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+});
